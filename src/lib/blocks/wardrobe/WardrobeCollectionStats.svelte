e3a69352dba5a757663b8423c1e47afcca7ecd16
SirArchibald <43316918+SirArchibald97@users.noreply.github.com>
2025-07-24 13:37:07 +0100

    Fixed some collections showing NaN reputation

--- src/lib/blocks/wardrobe/WardrobeCollectionStats.svelte
+++ src/lib/blocks/wardrobe/WardrobeCollectionStats.svelte
@@ -4,6 +4,8 @@
 
     const { player, collection }: { player: Player, collection: string } = $props();
     const cosmetics = player.collections?.cosmetics.filter(c => c.cosmetic.collection === collection);
+    const earnedRep = cosmetics?.filter(c => c.owned).reduce((acc, c) => acc + ((c.cosmetic.royalReputation?.reputationAmount) || 0 * c.donationsMade), 0) || 0;
+    const totalRep = cosmetics?.reduce((acc, c) => acc + ((c.cosmetic.royalReputation?.reputationAmount || 0) * (c.cosmetic.royalReputation?.donationLimit || 10)), 0) || 0;
 </script>
 
 <div class="w-full flex flex-col lg:grid lg:grid-cols-3 gap-2">
@@ -38,7 +40,7 @@
     <div class="flex gap-x-4 bg-neutral-900 rounded-md p-2">
         <span 
             class="flex justify-center items-center size-14 lg:size-18 bg-neutral-100 rounded-full {getCrownColour(player.crownLevel.levelData.level)}"  
-            style="background: conic-gradient({getCrownColourHex(player.crownLevel.levelData.level)} {Math.floor((cosmetics!.filter(c => c.owned && c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.donationsMade), 0) / cosmetics!.filter(c => c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.cosmetic.royalReputation.donationLimit), 0)) * 360)}deg, oklch(0.269 0 0) 0deg)"
+            style="background: conic-gradient({getCrownColourHex(player.crownLevel.levelData.level)} {Math.floor((earnedRep / totalRep) * 360)}deg, oklch(0.269 0 0) 0deg)"
         >
             <span class="flex justify-center items-center size-10 lg:size-14 bg-neutral-900 rounded-full">
                 <img src="https://cdn.islandstats.xyz/icons/currency/royal_reputation.png" alt="Royal Reputation Icon" class="size-6 lg:size-8" />
@@ -49,13 +51,10 @@
             <div class="flex gap-x-1 tabular-nums">
                 <img src="https://cdn.islandstats.xyz/icons/currency/royal_reputation.png" alt="Royal Reputation Icon" class="size-6 self-center" />
                 <p class="text-base tabular-nums">
-                    <span>{cosmetics!.filter(c => c.owned && c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.donationsMade), 0).toLocaleString()}</span>
+                    <span>{earnedRep.toLocaleString()}</span>
                     <span> / </span>
-                    <span>{cosmetics!.filter(c => c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.cosmetic.royalReputation.donationLimit), 0).toLocaleString()}</span>
-                    <span class="text-neutral-500">({calculatePercentage(
-                        cosmetics!.filter(c => c.owned && c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.donationsMade), 0),
-                        cosmetics!.filter(c => c.cosmetic.royalReputation).reduce((acc, c) => acc + (c.cosmetic.royalReputation.reputationAmount * c.cosmetic.royalReputation.donationLimit), 0)
-                    )}%)</span>
+                    <span>{totalRep.toLocaleString()}</span>
+                    <span class="text-neutral-500">({calculatePercentage(earnedRep, totalRep) || 100}%)</span>
                 </p>
             </div>
         </div>
