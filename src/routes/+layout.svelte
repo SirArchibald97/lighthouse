659655fb22f0052bc47abe5e70c6ef4f769cb19c
SirArchibald <archiemargretts@gmail.com>
2025-11-02 12:00:47 +0000

    Added announcement feature for lighthouse.gay expiration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
-    import '../app.css';
-    import Header from '$lib/blocks/Header.svelte';
+	import '../app.css';
+	import { page } from '$app/state';
+	import Header from '$lib/blocks/Header.svelte';
 	import Footer from '$lib/blocks/Footer.svelte';
+	import Announcement from '$lib/blocks/Announcement.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-    <link rel="icon" href="https://cdn.islandstats.xyz/emojis/crown.png" />
-    <title>Lighthouse by SirArchibald</title>
+	<link rel="icon" href="https://cdn.islandstats.xyz/emojis/crown.png" />
+	<title>Lighthouse by SirArchibald</title>
 </svelte:head>
-<div class="flex flex-col min-h-screen text-neutral-50 bg-neutral-900">
-    <Header />
+<div class="flex min-h-screen flex-col bg-neutral-900 text-neutral-50">
+	<Header />
 
-    <div class="flex-1">
-        {@render children()}
-    </div>
+	<div class="flex-1">
+		{#if page.url.hostname === 'lighthouse.gay' || page.url.hostname === 'localhost'}
+			<Announcement>
+				The domain <b>lighthouse.gay</b> will be unsupported after Dec 30th 2025. Please switch back
+				to
+				<b
+					><a class="text-blue-200 underline" href="https://stats.sirarchibald.dev"
+						>stats.sirarchibald.dev</a
+					></b
+				> to continue using Lighthouse. We apologise for the inconvenience.
+			</Announcement>
+		{/if}
 
-    <Footer />
+		{@render children()}
+	</div>
+
+	<Footer />
 </div>
